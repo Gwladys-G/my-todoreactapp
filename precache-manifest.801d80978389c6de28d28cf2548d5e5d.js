18283401853c9fa20146334a61b690a4af54880c
Gwladys Engels <gwladys.engels@gmail.com>
2022-09-02 17:37:18 -0400

    Updates

---
+++ precache-manifest.801d80978389c6de28d28cf2548d5e5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad5c1204041627a97d3e",
+    "url": "/my-todoreactapp/static/css/main.86f248d8.chunk.css"
+  },
+  {
+    "revision": "ad5c1204041627a97d3e",
+    "url": "/my-todoreactapp/static/js/main.ad5c1204.chunk.js"
+  },
+  {
+    "revision": "d9010b8d86b646b11a6b",
+    "url": "/my-todoreactapp/static/css/1.e860c9b1.chunk.css"
+  },
+  {
+    "revision": "d9010b8d86b646b11a6b",
+    "url": "/my-todoreactapp/static/js/1.d9010b8d.chunk.js"
+  },
+  {
+    "revision": "866db30de0223071156f",
+    "url": "/my-todoreactapp/static/js/runtime~main.866db30d.js"
+  },
+  {
+    "revision": "8195aac59c8be5f73cdfb46e7e7d1386",
+    "url": "/my-todoreactapp/index.html"
+  }
+];
